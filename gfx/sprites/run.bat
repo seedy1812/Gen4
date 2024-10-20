432d06825814ec360ddc860524ab5d36a42a4f76
seedy1812 <63559352+seedy1812@users.noreply.github.com>
2024-10-20 19:53:51 +0100

    Add files via upload

---
+++ gfx/sprites/run.bat
@@ -0,0 +1,17 @@
+gfx2next -sprites -colors-4bit line1.png
+gfx2next -sprites -colors-4bit line2.png
+gfx2next -sprites -colors-4bit line3.png
+gfx2next -sprites -colors-4bit line4.png
+gfx2next -sprites -colors-4bit line5a.png
+gfx2next -sprites -colors-4bit line6a.png
+
+
+gfx2next -sprites -colors-4bit line7/7_11.png
+gfx2next -sprites -colors-4bit line7/7_12.png
+gfx2next -sprites -colors-4bit line7/7_13.png
+gfx2next -sprites -colors-4bit line7/7_22.png
+gfx2next -sprites -colors-4bit line7/7_23.png
+gfx2next -sprites -colors-4bit line7/7_24.png
+gfx2next -sprites -colors-4bit line7/7_33.png
+gfx2next -sprites -colors-4bit line7/7_34.png
+gfx2next -sprites -colors-4bit line7/7_44.png
