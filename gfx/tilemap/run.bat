39d51501988e798190c9156c22c914f7b079c523
seedy1812 <63559352+seedy1812@users.noreply.github.com>
2024-10-20 19:51:19 +0100

    Add files via upload

---
+++ gfx/tilemap/run.bat
@@ -0,0 +1,8 @@
+gfx2next -tile-size=8x8 -tile-pal=0 -tile-offset=0  -colors-4bit -map-16bit line1.png
+gfx2next -tile-size=8x8 -tile-pal=1 -tile-offset=2  -colors-4bit -map-16bit line2.png
+gfx2next -tile-size=8x8 -tile-pal=2 -tile-offset=4  -colors-4bit -map-16bit line3.png
+gfx2next -tile-size=8x8 -tile-pal=3 -tile-offset=8  -colors-4bit -map-16bit line4.png
+gfx2next -tile-size=8x8 -tile-pal=4 -tile-offset=12 -colors-4bit -map-16bit line5.png
+gfx2next -tile-size=8x8 -tile-pal=5 -tile-offset=18 -colors-4bit -map-16bit line6.png
+gfx2next -tile-size=8x8 -tile-pal=6 -tile-offset=24 -colors-4bit -map-16bit line61.png
+gfx2next -tile-size=8x8 -tile-pal=7 -tile-offset=30 -colors-4bit -map-16bit -tile-norepeat gen4.png
